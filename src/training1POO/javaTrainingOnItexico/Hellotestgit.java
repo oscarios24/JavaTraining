63b1be6cbac9ee95e12a8e2182fc06eeaa5ca5e6
oscar <oscarios24@gmail.com>
2019-11-08 10:33:23 -0600

    Here is a new class

---
+++ src/training1POO/javaTrainingOnItexico/Hellotestgit.java
@@ -0,0 +1,14 @@
+package training1POO.javaTrainingOnItexico;
+
+public class Hellotestgit {
+
+	public Hellotestgit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
